8761f9206c38d636e7b11122e224fc8123fdb6c1
Lorenzo Masini <rugginoso@develer.com>
2013-07-24 07:57:53 +0200

    Better selector for technologies.

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from projects.models import Project, Technology
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+	filter_horizontal = ('technologies',)
+
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Technology)
